17a444cde7e04a1c6e7487a2d51bca054149f9d6
Tyler Combs <tyler@tlcombs.com>
2022-01-27 16:15:08 -0600

    blah

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import ReactPlayer from "react-player/youtube";
 import {
   Center,
   Container,
@@ -14,64 +15,79 @@ import {
   NumberInputField,
   Link,
   Button,
+  Box,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
-const doSomeStuff = () => {
-  console.log("Doing Stuff");
-};
-
-const InputForm = () => {
+const InputForm = (setupVideo) => {
   return (
-    <VStack>
-      <FormControl>
-        <FormLabel htmlFor="youtube-url">
-          <Link href="https://youtube.com" isExternal>
-            Youtube
-          </Link>{" "}
-          URL
-        </FormLabel>
-        <Input
-          id="youtube-url"
-          type="text"
-          placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        />
-      </FormControl>
+    <Box padding={16}>
+      <VStack>
+        <FormControl>
+          <FormLabel htmlFor="youtube-url">
+            <Link href="https://youtube.com" isExternal>
+              Youtube
+            </Link>{" "}
+            URL
+          </FormLabel>
+          <Input
+            id="youtube-url"
+            type="text"
+            placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          />
+        </FormControl>
 
-      <FormControl>
-        <FormLabel htmlFor="min">Start Minutes</FormLabel>
-        <NumberInput min={0} precision={0}>
-          <NumberInputField id="min" placeholder="2" />
-        </NumberInput>
-      </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="min">Start Minutes</FormLabel>
+          <NumberInput min={0} precision={0}>
+            <NumberInputField id="min" placeholder="2" />
+          </NumberInput>
+        </FormControl>
 
-      <FormControl>
-        <FormLabel htmlFor="sec">Start Seconds</FormLabel>
-        <NumberInput min={0} precision={5}>
-          <NumberInputField id="sec" placeholder="45.653" />
-        </NumberInput>
-        <FormHelperText>Be precise</FormHelperText>
-      </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="sec">Start Seconds</FormLabel>
+          <NumberInput min={0} precision={5}>
+            <NumberInputField id="sec" placeholder="45.653" />
+          </NumberInput>
+          <FormHelperText>Be precise.</FormHelperText>
+        </FormControl>
 
-      <FormControl>
-        <FormLabel htmlFor="dur">Duration Seconds</FormLabel>
-        <NumberInput min={0} max={10} precision={5}>
-          <NumberInputField id="dur" placeholder="2.1" />
-        </NumberInput>
-        <FormHelperText>Be precise</FormHelperText>
-      </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="dur">Duration Seconds</FormLabel>
+          <NumberInput min={0} max={10} precision={5}>
+            <NumberInputField id="dur" placeholder="2.1" />
+          </NumberInput>
+          <FormHelperText>Be precise.</FormHelperText>
+        </FormControl>
 
-      <Button colorScheme="purple" isFullWidth onClick={doSomeStuff}>
-        Submit
-      </Button>
-    </VStack>
+        <Button colorScheme="purple" isFullWidth onClick={setupVideo}>
+          Submit
+        </Button>
+      </VStack>
+    </Box>
   );
 };
 
 const Home = () => {
+  const [youtubeUrl, setYoutubeUrl] = useState("");
+  const [startMins, setStartMins] = useState(0);
+  const [startSecs, setStartSecs] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const setupVideo = () => {};
+
   return (
     <Center>
       <Container>
-        <InputForm />
+        <InputForm setupVideo />
+        <VStack>
+          <ReactPlayer
+            playing
+            controls
+            url="https://www.youtube.com/watch?v=Mh5LY4Mz15o?start=30&end=31"
+            youtubeConfig={{ playerVars: { start: 30, end: 35 } }}
+          />
+        </VStack>
       </Container>
     </Center>
   );

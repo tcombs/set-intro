b8ae88f0b904c831a74d883f4508c017366c9d08
Tyler Combs <tyler@tlcombs.com>
2022-01-27 13:03:32 -0600

    Buttons

--- pages/index.js
+++ pages/index.js
@@ -12,46 +12,66 @@ import {
   Input,
   NumberInput,
   NumberInputField,
+  Link,
+  Button,
 } from "@chakra-ui/react";
+
+const doSomeStuff = () => {
+  console.log("Doing Stuff");
+};
+
+const InputForm = () => {
+  return (
+    <VStack>
+      <FormControl>
+        <FormLabel htmlFor="youtube-url">
+          <Link href="https://youtube.com" isExternal>
+            Youtube
+          </Link>{" "}
+          URL
+        </FormLabel>
+        <Input
+          id="youtube-url"
+          type="text"
+          placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="min">Start Minutes</FormLabel>
+        <NumberInput min={0} precision={0}>
+          <NumberInputField id="min" placeholder="2" />
+        </NumberInput>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="sec">Start Seconds</FormLabel>
+        <NumberInput min={0} precision={5}>
+          <NumberInputField id="sec" placeholder="45.653" />
+        </NumberInput>
+        <FormHelperText>Be precise</FormHelperText>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="dur">Duration Seconds</FormLabel>
+        <NumberInput min={0} max={10} precision={5}>
+          <NumberInputField id="dur" placeholder="2.1" />
+        </NumberInput>
+        <FormHelperText>Be precise</FormHelperText>
+      </FormControl>
+
+      <Button colorScheme="purple" isFullWidth onClick={doSomeStuff}>
+        Submit
+      </Button>
+    </VStack>
+  );
+};
 
 const Home = () => {
   return (
     <Center>
       <Container>
-        <VStack>
-          <FormControl>
-            <FormLabel htmlFor="youtube-url">Youtube URL</FormLabel>
-            <Input
-              id="youtube-url"
-              type="text"
-              placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="min">Start Minutes</FormLabel>
-            <NumberInput min={0} precision={0}>
-              <NumberInputField id="min" />
-            </NumberInput>
-            <FormHelperText>e.g. 5</FormHelperText>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="sec">Start Seconds</FormLabel>
-            <NumberInput min={0} precision={5}>
-              <NumberInputField id="sec" />
-            </NumberInput>
-            <FormHelperText>e.g. 34.56</FormHelperText>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="dur">Duration Seconds</FormLabel>
-            <NumberInput min={0} max={10} precision={5}>
-              <NumberInputField id="dur" />
-            </NumberInput>
-            <FormHelperText>e.g. 2.45</FormHelperText>
-          </FormControl>
-        </VStack>
+        <InputForm />
       </Container>
     </Center>
   );
